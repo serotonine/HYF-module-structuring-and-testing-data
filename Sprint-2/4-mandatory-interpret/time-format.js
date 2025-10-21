1c8687e0526fd4ccb1d1d1d98d65b572d029d74c
serotonine <ks.serotonine@gmail.com>
2025-10-21 21:46:26 +0200

    Sprint2: 4-mandatory-interpret.

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -1,5 +1,6 @@
 function pad(num) {
-  return num.toString().padStart(2, "0");
+  const result = num.toString().padStart(2, "0"); 
+  return result;
 }
 
 function formatTimeDisplay(seconds) {
@@ -7,28 +8,29 @@ function formatTimeDisplay(seconds) {
   const totalMinutes = (seconds - remainingSeconds) / 60;
   const remainingMinutes = totalMinutes % 60;
   const totalHours = (totalMinutes - remainingMinutes) / 60;
+  console.log("remainingSeconds",remainingSeconds);
 
   return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
 }
-
-// You will need to play computer with this example - use the Python Visualiser https://pythontutor.com/visualize.html#mode=edit
-// to help you answer these questions
+console.log("formatTimeDisplay(61)", formatTimeDisplay(61));
 
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-// =============> write your answer here
+// 3 times.
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-// =============> write your answer here
+// 0
 
 // c) What is the return value of pad is called for the first time?
-// =============> write your answer here
+// "00"
 
-// d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// d) What is the value assigned to num when pad is called for the last time in this program?
+//  Explain your answer
+// Remaining seconds => 1 (61 seconds)
 
-// e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
-// =============> write your answer here
+// e) What is the return value assigned to num when pad is called for the last time in this program?
+//  Explain your answer
+// "01" => pad fill the string < 2 with 0.
