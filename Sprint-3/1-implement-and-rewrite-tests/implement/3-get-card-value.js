d706b68f1d143f2693d90e220e5c57a4dbec570d
serotonine <ks.serotonine@gmail.com>
2025-10-30 14:38:19 +0100

    Sprint#3: 3-get-card-value.

--- Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
+++ Sprint-3/1-implement-and-rewrite-tests/implement/3-get-card-value.js
@@ -6,11 +6,34 @@
 // the first test and first case is written for you
 // complete the rest of the tests and cases
 // write one test at a time, and make it pass, build your solution up methodically
-// just make one change at a time -- don't rush -- programmers are deep and careful thinkers
-function getCardValue(card) {
+// just make one change at a time -- don't rush -- programmers are deep and careful thinkers.
+
+/* function getCardValue(card) {
+  const rank = card.charAt(0);
   if (rank === "A") {
     return 11;
   }
+} */
+function getCardValue(card) {
+  if(typeof card !== 'string' || card.length > 2 || !["♠", "♣︎", "♦︎","♥"].includes(card.charAt(1)) ){
+    throw new Error("Invalid card rank.");
+  }
+  // Set values to be tested.
+  const rank = card.charAt(0);
+  const nb = +rank;
+  // Numbers range 2-10.
+  if(!isNaN(nb) && (nb >= 2 && nb <=10 ) ){
+    return nb;
+  }
+  // Ace.
+  else if (rank === "A") {
+    return 11;
+  // King Queen Valet.
+  } else if (["J", "Q", "K"].includes(rank)) {
+    return 10;
+  }
+  // Others.
+  throw new Error("Invalid card rank.");
 }
 
 // The line below allows us to load the getCardValue function into tests in other files.
@@ -26,6 +49,7 @@ function assertEquals(actualOutput, targetOutput) {
     `Expected ${actualOutput} to equal ${targetOutput}`
   );
 }
+
 // Acceptance criteria:
 
 // Given a card string in the format "A♠" (representing a card in blackjack - the last character will always be an emoji for a suit, and all characters before will be a number 2-10, or one letter of J, Q, K, A),
@@ -39,19 +63,34 @@ assertEquals(aceofSpades, 11);
 // When the function is called with such a card,
 // Then it should return the numeric value corresponding to the rank (e.g., "5" should return 5).
 const fiveofHearts = getCardValue("5♥");
-// ====> write your test here, and then add a line to pass the test in the function above
+assertEquals(fiveofHearts, 5);
 
 // Handle Face Cards (J, Q, K):
 // Given a card with a rank of "10," "J," "Q," or "K",
 // When the function is called with such a card,
 // Then it should return the value 10, as these cards are worth 10 points each in blackjack.
+const queenofHearts = getCardValue("Q♥");
+assertEquals(queenofHearts, 10);
 
 // Handle Ace (A):
 // Given a card with a rank of "A",
 // When the function is called with an Ace,
 // Then it should, by default, assume the Ace is worth 11 points, which is a common rule in blackjack.
+const aceofHeart = getCardValue("A♥");
+assertEquals(aceofHeart, 11);
 
 // Handle Invalid Cards:
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
+try{
+  const error1 = getCardValue("1♥");
+//assertEquals(error1, false);
+const error2 = getCardValue(67);
+//assertEquals(error2, false);
+
+}
+catch(e){
+  assertEquals(e.message, "Invalid card rank.");
+}
+
--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -2,12 +2,37 @@
 // We will use the same function, but write tests for it using Jest in this file.
 const getCardValue = require("../implement/3-get-card-value");
 
-test("should return 11 for Ace of Spades", () => {
+test("Should return 11 for Ace of Spades", () => {
   const aceofSpades = getCardValue("A♠");
   expect(aceofSpades).toEqual(11);
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("Should return the number range (2-10).", () => {
+  expect(() => getCardValue("5♣︎").toEqual(5));
+  expect(() => getCardValue("6♣︎").toEqual(6));
+  expect(() => getCardValue("4♣︎").toEqual(4));
+  expect(() => getCardValue("3♣︎").toEqual(3));
+});
 // Case 3: Handle Face Cards (J, Q, K):
+test("Should return 1° for Face Cards (J, Q, K).", () => {
+  expect(() => getCardValue("J♣︎").toEqual(10));
+  expect(() => getCardValue("Q♠").toEqual(10));
+  expect(() => getCardValue("K♣︎").toEqual(10));
+  expect(() => getCardValue("K♥").toEqual(10));
+});
 // Case 4: Handle Ace (A):
+test("Should return 1° for Ace (A).", () => {
+  expect(() => getCardValue("A♣︎").toEqual(11));
+  expect(() => getCardValue("A♠").toEqual(11));
+  expect(() => getCardValue("A♦︎").toEqual(11));
+  expect(() => getCardValue("A♥").toEqual(11));
+});
 // Case 5: Handle Invalid Cards:
+test("Should throw an error for invalid cards", () => {
+  expect(() => getCardValue("1♠")).toThrowError("Invalid card rank.");
+  expect(() => getCardValue("X♥")).toThrowError("Invalid card rank.");
+  expect(() => getCardValue("A")).toThrowError("Invalid card rank.");
+  expect(() => getCardValue("A♠♠")).toThrowError("Invalid card rank.");
+  expect(() => getCardValue(123)).toThrowError("Invalid card rank.");
+});
